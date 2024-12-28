30bc14c9b05789e02ee7c113760130e03de6f8ff
Morteza Heidari <mheidari@edu.lapinamk.fi>
2024-12-28 02:26:46 +0200

    JupyterTemplate for numpy assignment is done

--- FristAssignment/save_matrix.py
+++ FristAssignment/save_matrix.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 # Creating 15x15 matrix with random values
-matrix = np.random.rand(15, 15)
-print(matrix)
+matrix_saved = np.random.rand(15, 15)
+print(matrix_saved)
 
 # Saving matrix to file
-np.savetxt('FristAssignment/matrix.txt', matrix)
+np.savetxt('FristAssignment/matrix.txt', matrix_saved)
 
 # print the shape of the matrix
-print(matrix.shape)
+print(matrix_saved.shape)
